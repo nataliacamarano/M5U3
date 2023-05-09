70938b6a6116d9ffa39f2276dfc9d027294b97fa
Natalia Camarano <natalia.camarano@gmail.com>
2023-05-09 18:52:03 +0200

    create novedad

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,49 @@
 var express = require("express");
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModel");
 
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render("admin/novedades", {
     layout: "admin/layout",
     usuario: req.session.usuario,
+    novedades,
   });
+});
+
+router.get("/agregar", (req, res, next) => {
+  res.render("admin/agregar", {
+    layout: "admin/layout",
+  });
+});
+
+router.post("/agregar", async (req, res, next) => {
+  const { titulo, subtitulo, cuerpo } = req.body;
+  try {
+    if (titulo != "" && subtitulo != "" && cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect("/admin/novedades");
+    } else {
+      res.render("admin/agregar", {
+        layout: "admin/layout",
+        error: true,
+        message: "Todos los campos son requeridos",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("admin/agregar", {
+      layout: "admin/layout",
+      error: true,
+      message: "No se cargo la novedad",
+    });
+  }
+});
+
+router.get("eliminar/:id", async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadById(id);
+  res.redirect("/admin/novedades");
 });
 
 module.exports = router;
